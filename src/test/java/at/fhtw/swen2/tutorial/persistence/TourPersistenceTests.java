bcdfa9160ef3302fb1876d4b13977c331c923974
chenyifen <chenyifen@cvte.com>
2023-06-17 16:54:05 +0800

    add tour persistence tests

--- src/test/java/at/fhtw/swen2/tutorial/persistence/TourPersistenceTests.java
+++ src/test/java/at/fhtw/swen2/tutorial/persistence/TourPersistenceTests.java
@@ -1,25 +1,85 @@
 package at.fhtw.swen2.tutorial.persistence;
 
-import at.fhtw.swen2.tutorial.persistence.entities.TourEntity;
-import at.fhtw.swen2.tutorial.persistence.repositories.TourRepository;
+import at.fhtw.swen2.tutorial.service.dto.Tour;
+import at.fhtw.swen2.tutorial.service.impl.TourServiceImpl;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@Slf4j
 @SpringBootTest
 class TourPersistenceTests {
 
-	@Autowired
-	private TourRepository tourRepository;
+    @Autowired
+    private TourServiceImpl tourServiceImpl;
 
-	@Test
-	void testTourRepository() {
-		TourEntity maxi = TourEntity.builder()
-				.name("Maxi")
-				.build();
-		//TODO
-		tourRepository.save(maxi);
-		tourRepository.findAll().forEach(System.out::println);
-	}
+    @Test
+    @Transactional
+    void testAddTour() {
+        Tour tour = Tour.builder()
+                .name("TestTour1")
+                .transportType("Bike")
+                .tourDistance("1")
+                .routeInformation("Route")
+                .to("Street A")
+                .from("Street B")
+                .estimatedTime("1 hour")
+                .description("My Test Tour")
+                .build();
+        tourServiceImpl.addNew(tour);
+        List<Tour> list = tourServiceImpl.getTourList();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getName().equals("TestTour1")) {
+//                tourServiceImpl.delete(tour);
+                log.info("Test Pass");
+                assertTrue(true);
+                return;
+            }
+        }
+        //cant find Test Tour, test fail
+        fail();
+    }
+
+    @Test
+    @Transactional
+    void testDeleteTour() {
+        Tour tour = Tour.builder()
+                .name("TestTour2")
+                .transportType("Bike")
+                .tourDistance("1")
+                .routeInformation("Route")
+                .to("Street A")
+                .from("Street B")
+                .estimatedTime("1 hour")
+                .description("My Test Tour")
+                .build();
+        tourServiceImpl.addNew(tour);
+        tourServiceImpl.getTourList().forEach(p -> {
+            if (p.getName() == "TestTour2") {
+                tourServiceImpl.delete(tour);
+                PauseTransition pause = new PauseTransition(Duration.seconds(1));
+                pause.setOnFinished(event -> {
+                    List<Tour> list = tourServiceImpl.getTourList();
+                    for (int i = 0; i < list.size(); i++) {
+                        if (list.get(i).getName().equals("TestTour2")) {
+                            log.info("Test Pass");
+                            assertTrue(true);
+                            return;
+                        }
+                    }
+                });
+            }
+        });
+        assert true;
+    }
 
 }
